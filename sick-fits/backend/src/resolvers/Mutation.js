4b21b7da3fbfcb8ccff1589402f5f5971cd98ab1
David Neuman <david.m.neuman@gmail.com>
2019-09-28 15:44:07 -0400

    Create initial items for testing + learn Prisma Yoga flow

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const Mutations = {};
+const Mutations = {
+  // TODO: Check if they are logged in.
+  async createItem(parent, args, ctx, info) {
+    // We spread the arguments here instead of assigning data to args, since we need to assign a user to this later.
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info,
+    );
+    return item;
+  },
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+// Use this when you don't need to do any verification or authentication on the request. Just using Prisma's query as is.
+
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  items: forwardTo('db'),
+};
 
 module.exports = Query;
